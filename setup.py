b061651cb6d97499f12cf06bc6f241fbd124d3d0
肖齐梁 <xiaoql@hollycrm.com>
2023-10-24 18:41:12 +0800

    version='0.0.3'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tokenizers_pegasus',
-    version='0.0.2',
+    version='0.0.3',
     author='xiaoql',
     author_email='xiaoql@gmail.com',
     url='https://github.com/Qiliang/pegasus.git',
