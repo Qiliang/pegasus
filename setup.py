9a8ce825500d0f7295858410da665eb17f40e6e7
肖齐梁 <xiaoql@hollycrm.com>
2023-10-24 18:08:00 +0800

    markdown

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     author_email='xiaoql@gmail.com',
     url='https://github.com/Qiliang/pegasus.git',
     description=u'tokenizers for pegasus',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=['jieba', 'rouge',
                       'torch', 'numpy', 'six', 'unicodedata'],
